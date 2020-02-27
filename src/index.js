bb982c28153085cc5c9ff872e56445b46a069b1d
Marrlika <Marrlika@outlook.com>
2020-02-27 17:56:31 +0300

    feat: add the solution to typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length==0){
+    return 0;
+  }
+  return array.reduce((minNum, item) => (minNum>item)?item:minNum, Infinity);  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length==0){
+    return 0;
+  }
+  return array.reduce((maxNum, item) => (maxNum<item)?item:maxNum, -Infinity);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array)||array.length==0){
+    return 0;
+  }
+  return array.reduce((sum, item) => sum + item, 0)/array.length;
 }
